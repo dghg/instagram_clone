4c41413bfbc8ed272a4311063d2a534e937ef677
dghg <donghyeongzz@gmail.com>
2020-11-12 13:56:07 +0900

    .

--- src/public/js/story.js
+++ src/public/js/story.js
@@ -1,7 +1,11 @@
-var STORY_TIMER = 700000;
+var STORY_TIMER = 7000;
 
-window.onload = function(e) {
-    setTimeout(()=>{
-        location.href = '/';
-    }, STORY_TIMER);
-}
+window.onload = function (e) {
+  setTimeout(() => {
+    location.href = '/';
+  }, STORY_TIMER);
+};
+
+window.onclick = function (e) {
+  location.href = `/`;
+};
